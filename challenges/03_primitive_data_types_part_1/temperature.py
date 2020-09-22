b568dfdb7af96eea1ef3de03552788163bd2fa3c
mallahdiv <therealmd2@outlook.com>
2020-09-22 19:55:37 -0400

    Update on 9/22/20

---
+++ challenges/03_primitive_data_types_part_1/temperature.py
@@ -0,0 +1,20 @@
+#Challenge 1
+
+#variable
+celsius_100 = 100.0
+
+print(celsius_100)
+
+#variable Convert a temperature of 0 degrees fahrenheit to celsius
+celsius_0 = (0 -32) * 5/9
+
+print(celsius_0)
+
+#printing & Convert a temperature of 34.2 degrees fahrenheit to celsius 
+print((34.2 -32) * 5/9)
+
+#Convert a temperature of 5 degrees celsius to fahrenheit?
+print((5* 9/5) + 32)
+
+#Converted 85.1 F to C to compare if it was hotter than 30.2 C
+print((85.1-32) * 5/9)
