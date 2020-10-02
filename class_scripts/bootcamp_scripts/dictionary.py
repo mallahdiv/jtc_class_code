cae5753e4526cce09de107d281eb79b6db10ac56
mallahdiv <therealmd2@outlook.com>
2020-10-02 13:16:49 -0400

    Updated 10/2/20

---
+++ class_scripts/bootcamp_scripts/dictionary.py
@@ -0,0 +1,35 @@
+#dictionaries
+
+# blank_list = []
+
+# blank_dict = {}
+# print(type(blank_dict))
+
+# user_account = {
+# 	'username': 'pbloom',
+# 	'balance': 270.26,	
+# }
+
+# # print(user_account['username'])
+
+# #addin a new value
+# user_account['last_transation_date'] = '9/15/2020'
+# print(user_account)
+# print(user_account['balance'])
+# user_account['user_birthyear'] = 1993
+# print(user_account)
+
+# #reassiging a value to a key
+# user_account['balance'] = 500.00
+# print(user_account)
+
+# #remove a key value pair
+# user_account.pop('user_birthyear')
+# print(user_account)
+
+groceries_dict = {
+	'fruits': ['apples', 'bananas', 'cherries'],
+	'vegetables': ['carrots', 'parsley']
+}
+
+print(groceries_dict['fruits'])
