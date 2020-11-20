411467963d38674034b1066504d6fc564e784f08
mallahdiv <therealmd2@outlook.com>
2020-11-20 05:46:22 -0500

    Updated 11/20/20

--- class_scripts/bootcamp_scripts/logic.py
+++ class_scripts/bootcamp_scripts/logic.py
@@ -1,16 +1,16 @@
 # math comparisons
 # print(1 > 0 )
 
-# a = 0
-# b = 1
-# c = 1
+a = 0
+b = 1
+c = 1
 
-# print(f" is a equal to b?: {a==b}")
-# print(f" is a not equal to b?: {a != b}")
-# print(f"is a greater than b?: {a > b}")
-# print(f'is a lesser than b?: {a < b}')
-# print(f"is b greater than or equal to c?: {b >= c}")
-# print(f" is b less than or equal to c?: {b <=c}")
+print(f" is a equal to b?: {a==b}")
+print(f" is a not equal to b?: {a != b}")
+print(f"is a greater than b?: {a > b}")
+print(f'is a lesser than b?: {a < b}')
+print(f"is b greater than or equal to c?: {b >= c}")
+print(f" is b less than or equal to c?: {b <=c}")
 
 #integer vs floats
 # print(3 == 3.000)
